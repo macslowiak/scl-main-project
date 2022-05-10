2f82caa4710e1a81c30a442a2703a4c481632819
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-10 20:57:32 +0200

    Add base entity close #10

--- pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.spring.springcourselearningpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
