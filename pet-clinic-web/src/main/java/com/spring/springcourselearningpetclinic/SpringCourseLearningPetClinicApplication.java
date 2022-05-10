4a9d5c3a58d702adf3980e705a36e7e2cf1f4ad5
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-10 15:11:04 +0200

    Refoactor to multi-module maven project close #1

--- pet-clinic-web/src/main/java/com/spring/springcourselearningpetclinic/SpringCourseLearningPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/spring/springcourselearningpetclinic/SpringCourseLearningPetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCourseLearningPetclinicApplication {
+public class SpringCourseLearningPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCourseLearningPetclinicApplication.class, args);
+        SpringApplication.run(SpringCourseLearningPetClinicApplication.class, args);
     }
 
 }
