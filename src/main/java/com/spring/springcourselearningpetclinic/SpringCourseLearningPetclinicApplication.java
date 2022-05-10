463db6daa93ca8f9a105045f349596eae124d324
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-10 10:26:36 +0200

    Project initialization

---
+++ src/main/java/com/spring/springcourselearningpetclinic/SpringCourseLearningPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springcourselearningpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseLearningPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseLearningPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springcourselearningpetclinic/SpringCourseLearningPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springcourselearningpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseLearningPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
