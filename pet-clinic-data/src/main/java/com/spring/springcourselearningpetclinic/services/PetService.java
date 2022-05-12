bc0c8edac178db6608d28df380e8bf60120dd10d
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-12 08:20:22 +0200

    Refactor Service Interface to Common Base Interface close #11

--- pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.spring.springcourselearningpetclinic.services;
 
 import com.spring.springcourselearningpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.spring.springcourselearningpetclinic.services;
 
 import com.spring.springcourselearningpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
