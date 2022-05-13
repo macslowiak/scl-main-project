a04bfe5ae980d09f9fca6fbce9fd65946f8bfea9
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-13 08:52:30 +0200

    Add Speciality Map Service

---
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.spring.springcourselearningpetclinic.services;
+
+import com.spring.springcourselearningpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>  {
+
+}
