e4b9051d6bc291a98ec4dde1ed732f49e7e64283
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-14 18:04:51 +0200

    Create JPA and Map based Visit Service

---
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.spring.springcourselearningpetclinic.services;
+
+import com.spring.springcourselearningpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package com.spring.springcourselearningpetclinic.services.map;
+
+import com.spring.springcourselearningpetclinic.model.Pet;
+import com.spring.springcourselearningpetclinic.model.Visit;
+import com.spring.springcourselearningpetclinic.services.PetService;
+import com.spring.springcourselearningpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if (object.getPet() == null || object.getPet().getOwner() == null
+                || object.getPet().getId() == null || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit (no pet/no owner/owner id = null/pet id = null");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
