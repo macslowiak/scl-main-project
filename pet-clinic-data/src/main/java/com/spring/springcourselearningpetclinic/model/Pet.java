3e064cdaffe4a5306b5a1703333e61ca9ccadf7d
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-14 15:58:00 +0200

    Create JPA mapping for Owners and class related

--- pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Pet.java
@@ -1,12 +1,29 @@
 package com.spring.springcourselearningpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "brit_date")
     private LocalDate brithDate;
 
     public String getName() {
