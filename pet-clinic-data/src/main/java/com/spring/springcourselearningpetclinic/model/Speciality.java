e81ec8bd5b192008689f2369c8fb35edb567a5da
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-16 09:15:30 +0200

    Refactor using Project Lombok

--- pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/spring/springcourselearningpetclinic/model/Speciality.java
@@ -1,9 +1,20 @@
 package com.spring.springcourselearningpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "speciality")
 public class Speciality extends BaseEntity {
@@ -11,11 +22,4 @@ public class Speciality extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
